5fd1dd11f22009e2350aa2e0375a0ff2e2a437e0
Loki <lokinell@gmail.com>
2019-08-13 05:28:23 +0800

    Update docker-centos-install.sh
    upgrade docker-compose version

--- docker-centos-install.sh
+++ docker-centos-install.sh
@@ -4,5 +4,5 @@ sudo yum-config-manager --add-repo  https://download.docker.com/linux/centos/doc
 sudo yum makecache fast
 sudo yum install docker-ce -y
 sudo systemctl start docker.service
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
